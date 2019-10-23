c8ae705f4f0055238afa9f7556bcc2fd658b82cb
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-23 10:57:46 -0600

    Add patrons_by_exhibit_interest method

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -14,6 +15,23 @@ class Museum
   def recommended_exhibits(patron)
     @exhibits.find_all do |exhibit|
       patron.interests.any? { |interest| exhibit.name.include?(interest) }
+    end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def interests_by_patron(exhibit)
+    @patrons.find_all do |patron|
+      patron.interests.include?(exhibit)
+    end
+  end
+
+
+  def patrons_by_exhibit_interest
+    @exhibits.collect do |exhibit|
+      hash[exhibit] = interests_by_patron(exhibit)
     end
   end
 end
